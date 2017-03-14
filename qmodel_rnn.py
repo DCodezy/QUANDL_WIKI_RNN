4f90b062bd06fb852ef6d6cfebb1a4606f2826e0
DCodezy <christopherf@utexas.edu>
2017-03-14 18:29:22 -0500

    fix

--- qmodel_rnn.py
+++ qmodel_rnn.py
@@ -5,6 +5,7 @@ Uses Keras with tensorflow backend
 from __future__ import print_function
 import numpy as np
 import pandas as pd
+import psutil
 
 DATA_DIR = '~/Downloads/WIKI_PRICES_212.csv'
 TICKER_COL = 'ticker'
@@ -18,12 +19,13 @@ raw_df = pd.read_csv(DATA_DIR)
 raw_df = raw_df[[TICKER_COL, DATE_COL] + NPARRAY_COLUMNS]
 unique_tickers = raw_df[TICKER_COL].unique()
 # Transfer from pandas to dict of tickers to numpy arrays (dtype=float32)
-data_dict = {}
+data_in = []
 for (i, ticker) in enumerate(unique_tickers):
     temp_data = raw_df[raw_df.ticker == ticker].drop([TICKER_COL, DATE_COL], 1).as_matrix()
     if temp_data.shape[0] > MIN_DAYS_ACTIVE:
         temp_data = temp_data[CUT_FIRST_DAYS:]
-        data_dict[ticker] = temp_data.astype(np.float32)
+        data_in.append(temp_data.astype(np.float32))
+    print("Memory used: " + str(psutil.virtual_memory()[2]) + '%')
     if i % 100 == 0:
 	print(i)
 
