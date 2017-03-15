58608cd469b6cd75211ea5bb1b4d8d195b5782cf
DCodezy <christopherf@utexas.edu>
2017-03-14 21:40:45 -0500

    preprocessing

--- qmodel_rnn.py
+++ qmodel_rnn.py
@@ -21,13 +21,19 @@ unique_tickers = raw_df[TICKER_COL].unique()
 # Transfer from pandas to dict of tickers to numpy arrays (dtype=float32)
 data_in = []
 for (i, ticker) in enumerate(unique_tickers):
-    temp_data = raw_df[raw_df.ticker == ticker].drop([TICKER_COL, DATE_COL], 1).as_matrix()
+    temp_data = raw_df[raw_df.ticker == ticker] \
+                    .drop([TICKER_COL, DATE_COL], 1) \
+                    .as_matrix()
     if temp_data.shape[0] > MIN_DAYS_ACTIVE:
         temp_data = temp_data[CUT_FIRST_DAYS:]
         data_in.append(temp_data.astype(np.float32))
-    print("Memory used: " + str(psutil.virtual_memory()[2]) + '%')
     if i % 100 == 0:
-	print(i)
+	       print(i)
+           print("Memory used: " + str(psutil.virtual_memory()[2]) + '%')
+
+raw_df = []
+np.save('tickerNames.npy', np.array(unique_tickers))
+np.save('tickerData.npy', np.array(data_in))
 
 print('Starting columns: ' + str(len(raw_df.columns)))
 print('Ending columns: ' + str(len([TICKER_COL, DATE_COL] + NPARRAY_COLUMNS)))
